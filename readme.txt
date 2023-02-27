e05d8736bc825b7246cb660a97301af52a2ea1c1
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2023-02-27 08:04:24 +0100

    plugin info setup

---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Plugin Name ===
+Contributors: oghenemavo
+Donate link: https://example.com/
+Tags: logistics, deliveries
+Requires at least: 5.6
+Tested up to: 5.4
+Stable tag: 4.3
+Requires PHP: 7.0
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Plugin for enabling & managing logistics & deliveries for orders on eCommerce platforms
---
+++ shipbubble.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * Plugin Name:  ShipBubble
+ * Description:  Plugin for enabling & managing logistics & deliveries for orders on eCommerce platforms
+ * Plugin URI:   https://profiles.wordpress.org/oghenemavo
+ * Contributors: oghenemavo
+ * Author:       Mavi Onogomuho
+ * Author URI:   https://linkedin.com/in/mavi-onogomuho
+ * Tags:         delivery, logistics, eCommerce
+ * Version:      1.0
+ * Stable tag:   1.0
+ * Requires at least: 5.6
+ * Tested up to:      8.1
+ * Text Domain:  shipbubble
+ * Domain Path:  /languages
+ * License:      GPL v3 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+ // exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) 
+{
+	exit;
+}
