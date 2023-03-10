0d131804c5fb2a69c71aa906a3e511b9447e0eba
ayodejiabon <abonayodeji@gmail.com>
2023-03-10 23:41:02 +0100

    changes

--- readme.txt
+++ readme.txt
@@ -1,11 +1,52 @@
-=== Plugin Name ===
-Contributors: oghenemavo
-Donate link: https://example.com/
-Tags: logistics, deliveries
-Requires at least: 5.6
-Tested up to: 5.4
-Stable tag: 4.3
-Requires PHP: 7.0
-License: GPLv3 or later
-License URI: https://www.gnu.org/licenses/gpl-3.0.html
-Plugin for enabling & managing logistics & deliveries for orders on eCommerce platforms
+=== Shipbubble WooCommerce Shipping Rates ===
+Contributors: Shipbubble, Mavi Onogomuho
+Donate link: https://www.shipbubble.com/
+Tags: logistics, deliveries, shipping rates, multiple couriers, post purchase experience
+Requires at least: 4.0
+Tested up to: 8.1
+Stable tag: 1.0
+Requires PHP: 5.6
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+
+== Description ==
+
+Shipbubble is a platform that enables retailers to conveniently delight their customers with multiple shipping options, thereby increasing conversion rates.
+
+Features:
+
+* Access pre-negotiated courier rates across our 17+ partners.
+* Build customer trust by offering transparency at rates at checkout as customers can see delivery times as well as prices.
+* Access to both international and domestic carrier options.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/shipbubble` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+1. Generate and copy your Production API key from your shipbubble dashboard
+1. Use the Settings->Shipbubble screen to validate your API key.
+1. Once your API key is valid, you will be provided with a link to configure your shipbubble woocommerce settings
+1. Once your shipbubble woocommerce settings have been saved, you now have rates at checkout available to your customers
+
+== Frequently Asked Questions ==
+
+= Do i need to create an account on shipbubble =
+
+Yes, you would need to create an account to generate your API key. You can create your account here https://app.shipbubble.com/register
+
+= If you have any more questions =
+
+Please visit our website - https://shipbubble.com
+
+== Screenshots ==
+
+1. Get access to multiple courier rates at checkout
+2. Create a personalized branded tracking experience for your customers.
+
+== Changelog ==
+
+
+== Upgrade Notice ==
+
--- shipbubble.php
+++ shipbubble.php
@@ -1,264 +1,259 @@
-<?php 
+<?php
 
-	/**
-	 * Plugin Name:  ShipBubble
-	 * Description:  Plugin for enabling & managing logistics & deliveries for orders on eCommerce platforms
-	 * Plugin URI:   https://profiles.wordpress.org/oghenemavo
-	 * Contributors: oghenemavo
-	 * Author:       Mavi Onogomuho
-	 * Author URI:   https://linkedin.com/in/mavi-onogomuho
-	 * Tags:         delivery, logistics, eCommerce
-	 * Version:      1.0
-	 * Stable tag:   1.0
-	 * Requires at least: 5.6
-	 * Tested up to:      8.1
-	 * Text Domain:  shipbubble
-	 * Domain Path:  /languages
-	 * License:      GPL v3 or later
-	 * License URI:  https://www.gnu.org/licenses/gpl-3.0.txt
-	 */
+/**
+ * Contributors: Shipbubble, Mavi Onogomuho
+ * Donate link: https://www.shipbubble.com/
+ * Description:  Shipbubble is a platform that enables retailers to conveniently delight their customers with multiple shipping options, thereby increasing conversion rates
+ * Tags: logistics, deliveries, shipping rates, multiple couriers, post purchase experience
+ * Requires at least: 4.0
+ * Tested up to: 8.1
+ * Stable tag: 1.0
+ * Requires PHP: 5.6
+ *Text Domain:  shipbubble
+ * Domain Path:  /languages
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-	// exit if file is called directly
-	if ( ! defined( 'ABSPATH' ) ) 
-	{
-		exit;
+// exit if file is called directly
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+// if  admin area
+if (is_admin()) {
+	// include dependencies
+	require_once plugin_dir_path(__FILE__) . 'admin/wordpress/settings-menu.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/wordpress/settings-page.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/wordpress/settings-register.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/wordpress/settings-callback.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/wordpress/async-validate-auth.php';
+
+	// Woocommerce
+	// require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/shipping-settings.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/woocommerce/async-create-shipment.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/woocommerce/async-validate-address.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/woocommerce/enqueue-styles.php';
+}
+
+// includes
+require_once plugin_dir_path(__FILE__) . 'includes/constants.php';
+require_once plugin_dir_path(__FILE__) . 'includes/endpoints.php';
+require_once plugin_dir_path(__FILE__) . 'includes/core-methods.php';
+
+// public
+require_once plugin_dir_path(__FILE__) . 'public/async-checkout-couriers.php';
+
+
+// action on activation
+function shipbubble_on_activation()
+{
+	if (!current_user_can('activate_plugins')) return;
+
+	if (get_option('shipbubble_init')) {
+		$data = array('initialized' => true, 'account_status' => false);
+		update_option('shipbubble_init', $data);
+	} else {
+		$data = array('initialized' => true, 'account_status' => false);
+		add_option('shipbubble_init', $data);
 	}
+}
 
-	// if  admin area
-	if ( is_admin() ) 
-	{
-		// include dependencies
-		require_once plugin_dir_path( __FILE__ ) . 'admin/wordpress/settings-menu.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/wordpress/settings-page.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/wordpress/settings-register.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/wordpress/settings-callback.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/wordpress/async-validate-auth.php';
-		
-		// Woocommerce
-		// require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/shipping-settings.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/async-create-shipment.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/async-validate-address.php';
-		require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/enqueue-styles.php';
-	}
+register_activation_hook(__FILE__, 'shipbubble_on_activation');
 
-	// includes
-	require_once plugin_dir_path( __FILE__ ) . 'includes/constants.php';
-	require_once plugin_dir_path( __FILE__ ) . 'includes/endpoints.php';
-	require_once plugin_dir_path( __FILE__ ) . 'includes/core-methods.php';
+
+// action on deactivation
+function shipbubble_on_deactivation()
+{
+	if (!current_user_can('activate_plugins')) return;
+
+	$data = array('initialized' => false, 'account_status' => false);
+	update_option('shipbubble_init', $data);
+}
+
+register_deactivation_hook(__FILE__, 'shipbubble_on_deactivation');
+
+// default plugin options
+function shipbubble_options_default(): array
+{
+	return array(
+		'shipbubble_api_key'     	=> '',
+	);
+}
+
+function shipbubble_wc_options_default(): array
+{
+	return array(
+		'extra_charges' => '0',
+		'courier_list' =>  array('all'),
+		'shipping_price' => 'default',
+		'shipping_category' => '',
+		'user_can_ship' => 'no',
+		'activate_shipbubble' => 'no',
+	);
+}
+
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) return;
+
+add_action('plugins_loaded', 'shipbubble_wc_api_init', 11);
+
+function shipbubble_wc_api_init()
+{
+	// if( class_exists( 'WC_Payment_Gateway' ) ) {
+	// admin
+	require_once plugin_dir_path(__FILE__) . 'admin/woocommerce/shipping-settings.php';
+	require_once plugin_dir_path(__FILE__) . 'admin/woocommerce/orders.php';
 
 	// public
-	require_once plugin_dir_path( __FILE__ ) . 'public/async-checkout-couriers.php';
+	require_once plugin_dir_path(__FILE__) . 'public/woocommerce/checkout.php';
+	require_once plugin_dir_path(__FILE__) . 'public/woocommerce/enqueue-styles.php';
+	// }
+}
 
 
-	// action on activation
-	function shipbubble_on_activation() 
-	{
-		if ( ! current_user_can( 'activate_plugins' ) ) return;
-
-		if (get_option('shipbubble_init')) {
-			$data = array('initialized' => true, 'account_status' => false);
-			update_option( 'shipbubble_init', $data );
-		} else {
-			$data = array('initialized' => true, 'account_status' => false);
-			add_option( 'shipbubble_init', $data );
+// Disable Shipping methods if not in checkout page
+add_filter('woocommerce_package_rates', 'keep_shipping_methods_on_checkout', 100, 2);
+function keep_shipping_methods_on_checkout($rates, $package)
+{
+	if (!is_checkout()) {
+		// Loop through shipping methods rates
+		foreach ($rates as $rate_key => $rate) {
+			unset($rates[$rate_key]); // Remove
 		}
+	}
+	return $rates;
+}
 
+// Shipping packages
+add_filter('woocommerce_shipping_packages', 'keep_shipping_packages_on_checkout', 20, 1);
+add_filter('woocommerce_cart_shipping_packages', 'keep_shipping_packages_on_checkout', 20, 1);
+function keep_shipping_packages_on_checkout($packages)
+{
+	if (!is_checkout()) {
+		foreach ($packages as $key => $package) {
+			WC()->session->__unset('shipping_for_package_' . $key); // Remove
+			unset($packages[$key]); // Remove
+		}
+	}
+	return $packages;
+}
+
+// prevent proceed to order if shipping method has not been selected
+add_filter('woocommerce_order_button_html', 'disable_place_order_button_html');
+function disable_place_order_button_html($button)
+{
+	// HERE define your targeted shipping method id
+	$targeted_shipping_method = "flat_rate:14";
+
+	// Get the chosen shipping method (if it exist)
+	$chosen_shipping_methods = WC()->session->get('chosen_shipping_methods');
+
+	// If the targeted shipping method is selected, we disable the button
+	if (in_array($targeted_shipping_method, $chosen_shipping_methods)) {
+		$style  = 'style="background:Silver !important; color:white !important; cursor: not-allowed !important; text-align:center;"';
+		$text   = apply_filters('woocommerce_order_button_text', __('Place order', 'woocommerce'));
+		$button = '<a class="button" ' . $style . '>' . $text . '</a>';
+	}
+	return $button;
+}
+
+// Append custom checkout fields to db when order has been paid for
+add_action('woocommerce_checkout_update_order_meta', 'shipbubble_checkout_update_order_meta', 10, 1);
+function shipbubble_checkout_update_order_meta($order_id)
+{
+	$options = get_option(WC_SHIPBUBBLE_ID, shipbubble_wc_options_default());
+
+	$userCanShip = isset($options['user_can_ship']) ? sanitize_text_field($options['user_can_ship']) : 'no';
+
+	$requestToken = $_POST['request_token'];
+	$serviceCode = $_POST['shipbubble_service_code'];
+	$courierId = $_POST['shipbubble_courier_id'];
+
+	// initialize address
+	$streetAddress = $_POST['shipping_address_1'];
+	$city = $_POST['shipping_city'];
+	$stateTag = $_POST['shipping_state'];
+	$countryTag = $_POST['shipping_country'];
+
+	if (strlen($streetAddress) < 1) {
+		$streetAddress = $_POST['billing_address_1'];
 	}
 
-	register_activation_hook( __FILE__, 'shipbubble_on_activation' );
-
-
-	// action on deactivation
-	function shipbubble_on_deactivation() 
-	{
-		if ( ! current_user_can( 'activate_plugins' ) ) return;
-		
-		$data = array('initialized' => false, 'account_status' => false);
-		update_option( 'shipbubble_init', $data );
+	if (strlen($city) < 1) {
+		$city = $_POST['billing_city'];
 	}
 
-	register_deactivation_hook( __FILE__, 'shipbubble_on_deactivation' );
-
-	// default plugin options
-	function shipbubble_options_default(): array 
-	{
-		return array(
-			'shipbubble_api_key'     	=> '',
-		);
+	if (strlen($stateTag) < 1) {
+		$stateTag = $_POST['billing_state'];
 	}
 
-	function shipbubble_wc_options_default(): array 
-	{
-		return array(
-			'extra_charges' => '0',
-			'courier_list' =>  array('all'),
-			'shipping_price' => 'default',
-			'shipping_category' => '',
-			'user_can_ship' => 'no',
-			'activate_shipbubble' => 'no',
-		);
+	if (strlen($countryTag) < 1) {
+		$countryTag = $_POST['billing_country'];
 	}
 
-	if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) return;
+	$address = sb_create_address($streetAddress, $city, $stateTag, $countryTag);
 
-	add_action( 'plugins_loaded', 'shipbubble_wc_api_init', 11 );
+	// error_log(print_r($address, true));
+	// error_log(print_r(json_decode(json_decode($_POST['shipbubble_shipment_details'])), true));
+	// die;
 
-	function shipbubble_wc_api_init() {
-		// if( class_exists( 'WC_Payment_Gateway' ) ) {
-			// admin
-			require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/shipping-settings.php';
-			require_once plugin_dir_path( __FILE__ ) . 'admin/woocommerce/orders.php';
+	if (isset($_POST['shipbubble_shipment_details'])) {
 
-			// public
-			require_once plugin_dir_path( __FILE__ ) . 'public/woocommerce/checkout.php';
-			require_once plugin_dir_path( __FILE__ ) . 'public/woocommerce/enqueue-styles.php';
-		// }
-	}
-	
+		if (strtolower($userCanShip) == 'yes') {
 
-	// Disable Shipping methods if not in checkout page
-	add_filter( 'woocommerce_package_rates', 'keep_shipping_methods_on_checkout', 100, 2 );
-	function keep_shipping_methods_on_checkout( $rates, $package ) {
-		if ( ! is_checkout() ) {
-			// Loop through shipping methods rates
-			foreach( $rates as $rate_key => $rate ){
-				unset($rates[$rate_key]); // Remove
-			}
-		}
-		return $rates;
-	}
+			// error_log(print_r($requestToken, true));
+			// error_log(print_r($serviceCode, true));
+			// error_log(print_r($courierId, true));
 
-	// Shipping packages
-	add_filter( 'woocommerce_shipping_packages', 'keep_shipping_packages_on_checkout', 20, 1 );
-	add_filter( 'woocommerce_cart_shipping_packages', 'keep_shipping_packages_on_checkout', 20, 1 );
-	function keep_shipping_packages_on_checkout( $packages ) {
-		if ( ! is_checkout() ) {
-			foreach( $packages as $key => $package ) {
-				WC()->session->__unset('shipping_for_package_'. $key); // Remove
-				unset($packages[$key]); // Remove
-			}
-		}
-		return $packages;
-	}
+			// die;
 
-	// prevent proceed to order if shipping method has not been selected
-	add_filter('woocommerce_order_button_html', 'disable_place_order_button_html' );
-	function disable_place_order_button_html( $button ) {
-		// HERE define your targeted shipping method id
-		$targeted_shipping_method = "flat_rate:14";
+			if (isset($requestToken, $serviceCode, $courierId) && !empty($requestToken) && !empty($serviceCode) && !empty($courierId)) {
+				$shipmentPayload = array(
+					'request_token' => $requestToken,
+					'service_code' => $serviceCode,
+					'courier_id' => $courierId,
+				);
 
-		// Get the chosen shipping method (if it exist)
-		$chosen_shipping_methods = WC()->session->get('chosen_shipping_methods');
-		
-		// If the targeted shipping method is selected, we disable the button
-		if( in_array( $targeted_shipping_method, $chosen_shipping_methods ) ) {
-			$style  = 'style="background:Silver !important; color:white !important; cursor: not-allowed !important; text-align:center;"';
-			$text   = apply_filters( 'woocommerce_order_button_text', __( 'Place order', 'woocommerce' ) );
-			$button = '<a class="button" '.$style.'>' . $text . '</a>';
-		}
-		return $button;
-	}
+				$response = shipbubble_create_shipment($shipmentPayload);
 
-	// Append custom checkout fields to db when order has been paid for
-	add_action( 'woocommerce_checkout_update_order_meta', 'shipbubble_checkout_update_order_meta', 10, 1 );
-	function shipbubble_checkout_update_order_meta( $order_id ) 
-	{
-		$options = get_option( WC_SHIPBUBBLE_ID, shipbubble_wc_options_default() );
+				if (isset($response->response_code) && $response->response_code == HTTP_RESPONSE_OK) {
+					// set shipbubble order id
+					update_post_meta($order_id, 'shipbubble_order_id', $response->data->order_id);
 
-        $userCanShip = isset( $options['user_can_ship'] ) ? sanitize_text_field( $options['user_can_ship'] ) : 'no';
-
-		$requestToken = $_POST['request_token'];
-		$serviceCode = $_POST['shipbubble_service_code'];
-		$courierId = $_POST['shipbubble_courier_id'];
-
-		// initialize address
-		$streetAddress = $_POST['shipping_address_1'];
-		$city = $_POST['shipping_city'];
-		$stateTag = $_POST['shipping_state'];
-		$countryTag = $_POST['shipping_country'];
-
-		if (strlen($streetAddress) < 1) {
-			$streetAddress = $_POST['billing_address_1'];
-		}
-
-		if (strlen($city) < 1) {
-			$city = $_POST['billing_city'];
-		}
-
-		if (strlen($stateTag) < 1) {
-			$stateTag = $_POST['billing_state'];
-		}
-
-		if (strlen($countryTag) < 1) {
-			$countryTag = $_POST['billing_country'];
-		}
-
-		$address = sb_create_address($streetAddress, $city, $stateTag, $countryTag);
-
-		// error_log(print_r($address, true));
-		// error_log(print_r(json_decode(json_decode($_POST['shipbubble_shipment_details'])), true));
-		// die;
-		
-		if( isset( $_POST['shipbubble_shipment_details'] ) ) {
-
-			if (strtolower($userCanShip) == 'yes') {
-
-				// error_log(print_r($requestToken, true));
-				// error_log(print_r($serviceCode, true));
-				// error_log(print_r($courierId, true));
-
-				// die;
-
-				if (isset($requestToken, $serviceCode, $courierId) && !empty($requestToken) && !empty($serviceCode) && !empty($courierId) ) {
-					$shipmentPayload = array(
-						'request_token' => $requestToken,
-						'service_code' => $serviceCode,
-						'courier_id' => $courierId,
-					);
-	
-					$response = shipbubble_create_shipment($shipmentPayload); 
-
-					if (isset($response->response_code) && $response->response_code == HTTP_RESPONSE_OK) {
-						// set shipbubble order id
-						update_post_meta( $order_id, 'shipbubble_order_id', $response->data->order_id );
-	
-						// set shipping status
-						update_post_meta( $order_id, 'shipbubble_tracking_status', 'pending' );
-					}
+					// set shipping status
+					update_post_meta($order_id, 'shipbubble_tracking_status', 'pending');
 				}
-
-			} else {
-				// set shipbubble order id
-				update_post_meta( $order_id, 'shipbubble_order_id', '' );
-		
-				// set shipping status
-				update_post_meta( $order_id, 'shipbubble_tracking_status', '' );
 			}
-
-			// set shipbubble shipment details json
-		   update_post_meta( $order_id, 'shipbubble_shipment_details', $_POST['shipbubble_shipment_details'] );
-		   
 		} else {
-			$code = $serviceCode ?? 'speedaf-express';
-			update_post_meta( $order_id, 'shipbubble_shipment_details', json_encode(['service_code' => $code]) );
-
 			// set shipbubble order id
-			update_post_meta( $order_id, 'shipbubble_order_id', '' );
- 
+			update_post_meta($order_id, 'shipbubble_order_id', '');
+
 			// set shipping status
-			update_post_meta( $order_id, 'shipbubble_tracking_status', '' );
+			update_post_meta($order_id, 'shipbubble_tracking_status', '');
 		}
-		
-		// setting the delivery address
-		update_post_meta( $order_id, 'shipbubble_delivery_address', $address );
+
+		// set shipbubble shipment details json
+		update_post_meta($order_id, 'shipbubble_shipment_details', $_POST['shipbubble_shipment_details']);
+	} else {
+		$code = $serviceCode ?? 'speedaf-express';
+		update_post_meta($order_id, 'shipbubble_shipment_details', json_encode(['service_code' => $code]));
+
+		// set shipbubble order id
+		update_post_meta($order_id, 'shipbubble_order_id', '');
+
+		// set shipping status
+		update_post_meta($order_id, 'shipbubble_tracking_status', '');
 	}
 
-	function themeslug_enqueue_script() {
-		wp_enqueue_script( 'sweetalert2', 'https://cdn.jsdelivr.net/npm/sweetalert2@11', false );
-		// here you can enqueue more js / css files 
-	}
-	
-	add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_script' );
-	add_action( 'admin_enqueue_scripts', 'themeslug_enqueue_script' );
+	// setting the delivery address
+	update_post_meta($order_id, 'shipbubble_delivery_address', $address);
+}
 
-	
+function themeslug_enqueue_script()
+{
+	wp_enqueue_script('sweetalert2', 'https://cdn.jsdelivr.net/npm/sweetalert2@11', false);
+	// here you can enqueue more js / css files 
+}
+
+add_action('wp_enqueue_scripts', 'themeslug_enqueue_script');
+add_action('admin_enqueue_scripts', 'themeslug_enqueue_script');
