d07c53c85c04633f1abe787bb71efd36bdcb8134
Samson Moses <knomic10@gmail.com>
2025-05-26 02:08:01 +0100

    Tweaks

--- multi-vendor/dokan.php
+++ multi-vendor/dokan.php
@@ -1,6 +1,6 @@
 <?php
 
-if (is_shipbubble_dokan_multivendor_active()) {
+if (is_shipbubble_dokan_multivendor_active() && !is_shipbubble_admin_page()) {
     add_action('dokan_settings_after_store_phone', 'add_shipbubble_multivendor_form', 10, 2);
 	add_action('dokan_store_profile_saved', 'shipbubble_handle_saved_profile', 10, 2);
 	add_action('dokan_settings_before_form', 'shipbubble_add_address_validate_notice', 10, 2);
